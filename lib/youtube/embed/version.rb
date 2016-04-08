5c9f34b910dd89b62883c2d0430c7f1aee70fbc9
Ildar Shaynurov <shaynurov@gmail.com>
2016-04-08 16:37:47 +0300

    Initial commit

---
+++ lib/youtube/embed/version.rb
@@ -0,0 +1,5 @@
+module Youtube
+  module Embed
+    VERSION = "0.0.1"
+  end
+end
---
+++ spec/youtube/embed_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Youtube::Embed do
+  it 'has a version number' do
+    expect(Youtube::Embed::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
